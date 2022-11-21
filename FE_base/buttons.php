6d13176f2204145227126e5e81ee8f4b52aad921
nhatluu03 <115528919+nhatluu03@users.noreply.github.com>
2022-11-21 09:51:30 +0700

    Create buttons.php

---
+++ FE_base/buttons.php
@@ -0,0 +1,77 @@
+.btn-s-size {
+    min-width: 100px;
+    font-size: 1.4rem;
+    padding: 16px 0;
+    border-radius: 30px;
+}
+
+.btn-m-size {
+    min-width: 180px;
+    font-size: 1.8rem;
+    font-weight: 600;
+    padding: 12px 0;
+    border-radius: 4px;
+}
+
+.btn-l-size {
+    min-width: 190px;
+    font-size: 2rem;
+    font-weight: 600;
+    padding: 24px 0;
+    border-radius: 50px;
+}
+
+.btn-hover {
+    color: #fff;
+    cursor: pointer;
+    text-align: center;
+    border: none;
+    background-size: 300% 100%;
+    -o-transition: all 0.4s ease-in-out;
+    -webkit-transition: all 0.4s ease-in-out;
+    transition: all 0.4s ease-in-out;
+}
+
+.btn-hover:hover {
+    background-position: 100% 0;
+    -o-transition: all 0.4s ease-in-out;
+    -webkit-transition: all 0.4s ease-in-out;
+    transition: all 0.4s ease-in-out;
+}
+
+.btn-hover:focus {
+    outline: none;
+}
+
+.btn-hover.color-9 {
+    background-image: linear-gradient(
+        to right,
+        #25aae1,
+        #4481eb,
+        #04befe,
+        #3f86ed
+        );
+    box-shadow: 0px 4px 5px rgb(65 132 234 / 50%);
+}
+
+.btn-hover.color-2 {
+    background-image: linear-gradient(
+        to right,
+        #faa11c,
+        #e43603,
+        #fa7199,
+        #e85a19
+        );
+    box-shadow: 0px 4px 5px rgba(229, 66, 10, 50%);
+}
+
+.btn-hover.color-1 {
+    background-image: linear-gradient(
+        to right,
+        #25aae1,
+        #40e495,
+        #30dd8a,
+        #2bb673
+        );
+    box-shadow: 0px 4px 5px rgba(49, 196, 190, 50%);
+}
